43229e63ec2793cf7b7a73e5e1302b435dfcd8a3
Student6 <Student6@10.8.33.136>
2017-05-05 13:49:44 -0400

    Progress 0.1

---
+++ PlagueS/src/Climate.java
@@ -0,0 +1,23 @@
+
+
+public class Climate {
+
+	private double averageTemp;
+	
+	public static final Climate WARM_CLIMATE = new Climate(80);
+	public static final Climate COLD_CLIMATE = new Climate(40);
+	
+	public Climate(double avgT) {
+		averageTemp = avgT;
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getAverageTemp() {
+		return averageTemp;
+	}
+
+	public void setAverageTemp(double averageTemp) {
+		this.averageTemp = averageTemp;
+	}
+
+}
---
+++ PlagueS/src/State.java
@@ -0,0 +1,65 @@
+
+
+public class State {
+	
+	private String name;
+	private Climate climate;
+	private int population;
+	private boolean infected;
+	private boolean destroyed;
+	
+	private final int POPULATION_US = getPopulation();
+	
+	private String[] names = {"California","Texas","Florida","New York","Pennsylvania","Illinois","Ohio","Georgia","North Carolina","Michigan","New Jersey","Virginia","Washington","Arizona","Massachusetts","Tennessee","Indiana","Missouri","Maryland","Wisconsin","Colorado","Minnesota","South Carolina","Alabama","Louisiana","Kentucky","Oregon","Oklahoma","Connecticut","Puerto Rico","Iowa","Utah","Mississippi","Arkansas","Nevada","Kansas","New Mexico","Nebraska","West Virginia","Idaho","Hawaii","New Hampshire","Maine","Rhode Island","Montana","Delaware","South Dakota","North Dakota","Alaska","Wyoming","Vermont"};
+	//location of the state name of images
+	
+
+	public State(String name, int population, Climate climate) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.climate = climate;
+		this.population = population;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Climate getClimate() {
+		return climate;
+	}
+
+	public void setClimate(Climate climate) {
+		this.climate = climate;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	public boolean isInfected() {
+		return infected;
+	}
+
+	public void setInfected(boolean infected) {
+		this.infected = infected;
+	}
+
+	public boolean isDestroyed() {
+		return destroyed;
+	}
+
+	public void setDestroyed(boolean destroyed) {
+		this.destroyed = destroyed;
+	}
+
+
+}
