2576de70697bbbc11427734d0f7f42e20390f083
Student 8 <Student 8@10.8.33.103>
2017-05-09 13:29:03 -0400

    Started basic layout

---
+++ PlagueS/src/Ivan/Buttons.java
@@ -0,0 +1,17 @@
+package Ivan;
+
+public class Buttons{
+	private final static int Width = 25;
+	private final static int Height = 25;
+	public Buttons() {
+		super();
+	}
+	
+	public boolean isHovered(int x, int y){
+		
+		
+		return false;
+		
+	}
+
+}
---
+++ PlagueS/src/Ivan/Spawn.java
@@ -0,0 +1,14 @@
+package Ivan;
+
+public class Spawn {
+
+	public Spawn() {
+		// TODO Auto-generated constructor stub
+	}
+	/*
+	 *Will spawn virus points in one of the 50 states 
+	 * There shouldn't be more than one virus point on each state at a time
+	 * ^ Will require booleans on each state 
+	 * Will respawn if time runs out on the virus points as there is a timelimit
+	 */
+}
