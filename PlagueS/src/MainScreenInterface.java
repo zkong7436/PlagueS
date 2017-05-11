562dd8ad145d1fffa92d1a27e736af3a1e4ad8e4
Student8 <Student8@10.8.33.108>
2017-05-11 13:21:05 -0400

    interface

---
+++ PlagueS/src/MainScreenInterface.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+import GUIpractice.components.Visible;
+
+public interface MainScreenInterface {
+
+	public abstract void initRemainingItems(ArrayList<Visible> viewObjects);
+}
