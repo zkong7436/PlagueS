ff7c089322c7fbe9f6b19733feb1ad569b0cbce5
TheJoeJoe <jhur4626@bths.edu>
2017-05-16 13:53:53 -0400

    Update CureScreen.java

--- PlagueS/src/JosephIvan/CureScreen.java
+++ PlagueS/src/JosephIvan/CureScreen.java
@@ -14,5 +14,12 @@ public class CureScreen extends guiTeacher.GUIApplication{
 		DemoScreen cure = new DemoScreen(1000,1000);
 		setScreen(cure);		
 	}
+	public static void main (String[] args){
+		DemoScreen D = new DemoScreen(1000,1000);
+		Thread app = new Thread(D);
+		app.start();
+		
+	}
 
 }
+
