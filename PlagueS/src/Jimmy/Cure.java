e9b6b71acfb7f71a49bbc3bda2df918004e61ed5
TheJoeJoe <jhur4626@bths.edu>
2017-05-16 13:36:23 -0400

    Update Cure.java

--- PlagueS/src/Jimmy/Cure.java
+++ PlagueS/src/Jimmy/Cure.java
@@ -4,13 +4,15 @@ import Wendy.State;
 
 public class Cure {
 	
-	private double cure;
+	private double cure= 0;
 	private double infectedPercentage;
 	private double popDeadPercentage;
-	private int infected;
+	private int infected = 100;
 	public boolean activate = false;
-	
-	
+	private int counter = 0;
+	public static void main (String[] args){
+		
+	}
 	
 	public Cure() {
 		// TODO Auto-generated constructor stub
@@ -20,13 +22,16 @@ public class Cure {
 	public boolean isDetected(){
 		//while every second passes
 		//infected = State.getInfectedpop
-		int counter = 0;
+		
 		if(infected >= 20 || popDeadPercentage >= 3) {
+			System.out.println("cure has started");
+			infected = 10;
 			//say cure starts
 			//run cure method counterr
 		}
-		if(infected >= 10 && counter == 0){
+		if(infected >= 10 && counter < 1){
 				//say disease found in u.s
+			System.out.println("cure has ENDED");
 			counter++;
 		}
 		return false;
