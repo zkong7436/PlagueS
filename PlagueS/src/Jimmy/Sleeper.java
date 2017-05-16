315792ba80b0603081304613d086e0f3291cd37b
Student 8 <Student 8@10.8.33.103>
2017-05-16 13:55:03 -0400

    xdd

--- PlagueS/src/Jimmy/Sleeper.java
+++ PlagueS/src/Jimmy/Sleeper.java
@@ -10,7 +10,9 @@ public class Sleeper implements Runnable {
 		private boolean gameOn = true;
 		private boolean cureStarted = false;
 		private String[] months = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-		private String[] daysPerMonth = {"32","28","31","31","31","31","31","31","31","31","31","31"};
+		private int[] daysPerMonth = {31,28,31,31,31,31,31,31,31,31,3,31};
+		private int year = 2017;
+		private int dayYear;
 		private Cure cure;
 		private State state;
 		
@@ -32,9 +34,22 @@ public class Sleeper implements Runnable {
 				while(gameOn){
 					Thread.sleep(second);
 					days++;
-					System.out.println("Day " +days);
-					cure.isDetected();
+					dayYear++;
+					for(int i = 0; i > months.length; i++){
+						for(int j = 0; j > 31; j++){
+						if(daysPerMonth[j] > days){
+							days = 1;
+							i--;
+						}
+						if(dayYear > 365){
+							dayYear = 1;
+							year++;
+						}
+						System.out.println("Day " +days + "Month" + months[i] + "Year" + year);
+						cure.isDetected();
+					}
 				}
+			}
 				
 	
 			} catch (InterruptedException e) {
