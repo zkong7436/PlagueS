ee64fe108ffa13460df44bf8ede91c9ef28ff8b5
L0hL <izhen7813@bths.edu>
2017-06-02 13:50:54 -0400

    Update WorldPage.java

--- PlagueS/src/JosephIvan/WorldPage.java
+++ PlagueS/src/JosephIvan/WorldPage.java
@@ -12,14 +12,35 @@ private TextLabel text;
 
 	public WorldPage(int x, int y, int w, int h) {
 		super(x, y, w, h);
-		chart = new PieChart(20,20);
-		text = new TextLabel(0,0,300,40,"The Plague is starting to take over the world!");
-		text.setSize(35);
-		update();
+
+		
+		Thread update = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				
+				while(true){
+					update();
+					try {
+						Thread.sleep(50);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+
+		}
+				);
+		update.start();
 	}
 
 	@Override
 	public void update(Graphics2D g) {
+		chart = new PieChart(20,20);
+		text = new TextLabel(0,0,300,40,"The Plague is starting to take over the world!");
+		text.setSize(35);
 		if(chart!=null){
 			g.drawImage(text.getImage(), text.getX(),text.getY(),null);
 			g.drawImage(chart.getImage(), chart.getX(),chart.getY(),null);
@@ -28,3 +49,4 @@ private TextLabel text;
 	}
 
 }
+
