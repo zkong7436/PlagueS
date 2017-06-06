648c3440cb8393109f04a173cf451d95d54672e6
L0hL <izhen7813@bths.edu>
2017-06-06 13:44:21 -0400

    Update WorldPage.java

--- PlagueS/src/JosephIvan/WorldPage.java
+++ PlagueS/src/JosephIvan/WorldPage.java
@@ -7,8 +7,8 @@ import guiTeacher.components.TextLabel;
 public class WorldPage extends guiTeacher.components.Component {
 
 private PieChart chart;
-private BarGraph bar;
 private TextLabel text;
+private PieChart2 chart2;
 
 	public WorldPage(int x, int y, int w, int h) {
 		super(x, y, w, h);
@@ -39,14 +39,16 @@ private TextLabel text;
 	@Override
 	public void update(Graphics2D g) {
 		chart = new PieChart(20,20);
+		chart2 = new PieChart2(200,200);
 		text = new TextLabel(0,0,300,40,"The Plague is starting to take over the world!");
 		text.setSize(35);
 		if(chart!=null){
 			g.drawImage(text.getImage(), text.getX(),text.getY(),null);
 			g.drawImage(chart.getImage(), chart.getX(),chart.getY(),null);
+			g.drawImage(chart2.getImage(), chart2.getX(),chart2.getY(),null);
+			
 		}
 		
 	}
 
 }
-
