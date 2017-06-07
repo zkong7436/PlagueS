24fdf0b7f4ae7c46fda35fd9ebd3a73c98e6972c
zkong7436 <zkong7436@bths.edu>
2017-06-07 17:21:16 -0400

    Update DiseaseScreen.java

--- PlagueS/src/Tracey/DiseaseScreen.java
+++ PlagueS/src/Tracey/DiseaseScreen.java
@@ -6,6 +6,7 @@ package Tracey;
 import java.awt.Color;
 import java.util.List;
 
+import guiTeacher.components.TextColoredLabel;
 import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 
@@ -31,10 +32,25 @@ public class DiseaseScreen extends Upgrades {
 	@Override
 	public void initItems(List<Visible> viewObjects) {
 		//name entered in the beginning 
-		TextLabel name = new TextLabel(100, 200, 100, 500, "Bacteria" );
+		TextLabel name = new TextLabel(70, 200, 200, 700, "Bacteria");
 		name.setCustomTextColor(Color.white);
+		name.setSize(40);
+		
+		//separate lines
+		//add the starting country
+		//daily infections
+		//daily deaths
+		TextColoredLabel data = new TextColoredLabel(70, 270, 200, 40, "Start Location:", Color.black, Color.white);
+		data.setSize(20);
+		TextColoredLabel loca = new TextColoredLabel(70, 300, 200, 40, "Korea", Color.black, Color.white);
+		loca.setSize(18);
+		TextColoredLabel data2 = new TextColoredLabel(70, 500, 300, 200, "Start Location" + System.lineSeparator() + "Korea", Color.black, Color.white);
+		data.setSize(20);
 
 		viewObjects.add(name);
+		viewObjects.add(data);
+		viewObjects.add(loca);
+		viewObjects.add(data2);
 	}
 
 }
